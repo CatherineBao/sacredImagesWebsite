9e4d10aa3fea0222db9a4d488a01cc2686c91d38
Catherine Bao <112794244+CosmixCity@users.noreply.github.com>
2022-11-13 04:15:20 +0000

    fixed update spacing and updates

--- src/pages/Updates.jsx
+++ src/pages/Updates.jsx
@@ -20,15 +20,31 @@ function Updates() {
   	    </div>
   			<div className='flex justify-center flex-col items-center justify-center p-10 xl:px-52 w-full gap-10'>
   				<h1 className='text-xl lg:text-3xl font-bold p-3 lg:px-20 pt-10'>Update Information</h1>
-					<div className='w-1/4'>
+          <div className='md:w-1/4 w-full text-center md:text-left'>
+            <h2 className='font-bold'>11/12/2022</h2>
+    				<ul className='md:list-disc'>
+    					<li>Translated website framework into ReactJS</li>
+              <li>Added Image Gallery and Design Process content</li>
+              <li>Interactive mural</li>
+    				</ul>
+          </div>
+          <div className='md:w-1/4 w-full text-center md:text-left'>
+            <h2 className='font-bold'>8/12/2022</h2>
+    				<ul className='md:list-disc'>
+    					<li>Hamburger navigation for mobile</li>
+              <li>Added sociology stories</li>
+              <li>Added a student's story in AP Spanish</li>
+    				</ul>
+          </div>
+					<div className='md:w-1/4 w-full text-center md:text-left'>
             <h2 className='font-bold'>5/20/2022</h2>
-    				<ul className='list-disc'>
+    				<ul className='md:list-disc'>
     					<li>Uploaded Documentary</li>
     				</ul>
           </div>
-          <div className='w-1/4'>
+          <div className='md:w-1/4 w-full text-center md:text-left mb-10'>
             <h2 className='font-bold'>3/6/2022</h2>
-    				<ul className='list-disc'>
+    				<ul className='md:list-disc'>
     					<li>Added updates section</li>
     					<li>Added updates section to footer</li>
     					<li>Fixed a student's story in AP Spanish</li>
